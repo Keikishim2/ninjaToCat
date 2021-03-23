16e6bd678ce633b78c65179a22fe8ed2a68e7a09
Keikishim2 <67738474+Keikishim2@users.noreply.github.com>
2021-03-23 11:15:41 +0800

    Add files via upload

---
+++ j2.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+    var pic = 0;  
+    $(".img1").click(function(){
+    if(pic == 0) {
+        $(".img1").attr("src","ninja0.png");
+        pic = 1;
+    }
+    else if(pic == 1) {
+        $(".img1").attr("src","cat0.png");
+        pic = 0;
+    }
+    });
+});
+$(document).ready(function(){
+    var pic1 = 0;  
+    $(".img2").click(function(){
+    if(pic1 == 0) {
+        $(".img2").attr("src","ninja1.png");
+        pic1 = 1;
+    }
+    else if(pic1 == 1) {
+        $(".img2").attr("src","cat1.png");
+        pic1 = 0;
+    }
+    });
+});
+$(document).ready(function(){
+    var pic2 = 0;  
+    $(".img3").click(function(){
+    if(pic2 == 0) {
+        $(".img3").attr("src","ninja2.png");
+        pic2 = 1;
+    }
+    else if(pic2 == 1) {
+        $(".img3").attr("src","cat2.png");
+        pic2 = 0;
+    }
+    });
+});
+$(document).ready(function(){
+    var pic3 = 0;  
+    $(".img4").click(function(){
+    if(pic3 == 0) {
+        $(".img4").attr("src","ninja3.png");
+        pic3 = 1;
+    }
+    else if(pic3 == 1) {
+        $(".img4").attr("src","cat3.png");
+        pic3 = 0;
+    }
+    });
+});
+$(document).ready(function(){
+    var pic4 = 0;  
+    $(".img5").click(function(){
+    if(pic4 == 0) {
+        $(".img5").attr("src","ninja4.png");
+        pic4 = 1;
+    }
+    else if(pic4 == 1) {
+        $(".img5").attr("src","cat4.png");
+        pic4 = 0;
+    }
+    });
+});
+
+
+
